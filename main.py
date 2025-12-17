e58872d95060701b80b9346970c1a8605ecd9c6d
SprinkleSome <athmyridis@gmail.com>
2025-12-17 23:54:18 +0200

    improved function separation

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
         text_in_file = f.read()
-        return get_num_words(text_in_file)
+        return text_in_file
+
+
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
-    print(f"Found {text} total words")
+    words = get_num_words(text)
+    print(f"Found {words} total words")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(text):
     return len(text.split())
 
+def get_char_list(text):
+    char_list = {}
+    for char in text:
+        lower_case = char.lower()
+        if lower_case in char_list:
+            char_list[lower_case] += 1
+        else:
+            char_list[lower_case] = 1
+    return char_list

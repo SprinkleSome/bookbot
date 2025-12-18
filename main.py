66b56e4506b5b0407593678fe301c7e3f668faf2
SprinkleSome <athmyridis@gmail.com>
2025-12-18 10:10:16 +0200

    filepath added as argument/not hardcoded

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import get_num_words, get_char_list, sorted_list
+import sys
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +11,12 @@ def get_book_text(filepath):
 
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        text = get_book_text(sys.argv[1])
+
     words = get_num_words(text)
     char = get_char_list(text)
     the_list = sorted_list(char)
